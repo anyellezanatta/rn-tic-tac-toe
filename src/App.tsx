ca0f676cc3c891ed868d1ad15dd5c084f0ba89d9
Anyelle Zanatta <11357820+anyellezanatta@users.noreply.github.com>
2024-06-28 21:41:16 +0200

    chore: adds fonts, fixes types

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import "./theme/unistyles";
+
+import { Text, View } from "react-native";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 import { StatusBar } from "expo-status-bar";
 
+import { IconButton } from "@/components/IconButton";
+
 export const App = () => {
+  const { styles } = useStyles(stylesheet);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <IconButton />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
+const stylesheet = createStyleSheet((theme) => ({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.colors.darkNavy,
     alignItems: "center",
     justifyContent: "center",
   },
-});
+}));
