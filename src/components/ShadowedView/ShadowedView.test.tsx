fbd5496fc3b587ffafa32d9b1bc97eb562abd82f
Anyelle Zanatta <11357820+anyellezanatta@users.noreply.github.com>
2024-10-20 22:34:19 +0200

    fix: fixes shadowed view tests

--- src/components/ShadowedView/ShadowedView.test.tsx
+++ src/components/ShadowedView/ShadowedView.test.tsx
@@ -7,27 +7,44 @@ import { ShadowedView } from "./ShadowedView";
 
 describe("ShadowedView", () => {
   it("should render correctly color when set the color theme", () => {
-    render(<ShadowedView size="primary" color="semiDarkNavy" />);
+    render(
+      <ShadowedView
+        size="primary"
+        color="semiDarkNavy"
+        testID="ShadowedView"
+      />,
+    );
 
-    expect(screen.getByLabelText("ShadowedView")).toHaveStyle({
+    expect(screen.getByTestId("ShadowedView")).toHaveStyle({
       backgroundColor: theme.shadows.semiDarkNavy,
     });
   });
 
   it("should render size correctly when set the size theme", () => {
-    render(<ShadowedView size="secondary" color="semiDarkNavy" />);
+    render(
+      <ShadowedView
+        size="secondary"
+        color="semiDarkNavy"
+        testID="ShadowedView"
+      />,
+    );
 
-    expect(screen.getByLabelText("ShadowedView")).toHaveStyle({
+    expect(screen.getByTestId("ShadowedView")).toHaveStyle({
       paddingBottom: theme.spacing.$1,
     });
   });
 
   it("should render correctly when set false to isShadowed prop", () => {
     render(
-      <ShadowedView size="primary" color="semiDarkNavy" isShadowed={false} />,
+      <ShadowedView
+        size="primary"
+        color="semiDarkNavy"
+        isShadowed={false}
+        testID="ShadowedView"
+      />,
     );
 
-    expect(screen.getByLabelText("ShadowedView")).toHaveStyle({
+    expect(screen.getByTestId("ShadowedView")).toHaveStyle({
       paddingBottom: 0,
     });
   });
